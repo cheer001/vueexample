a91c6b61274a5e95e947353030993a21f45c5ee0
gerry <gerrycheer@gmail.com>
2020-05-01 16:33:40 +0800

    添加案例：
    vue传值：
    eventBus

---
+++ src/views/EventBus/child.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="container">
+    <h2>child</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log(this.EventBus.message);
+    // -> 'hello'
+    this.EventBus.$emit("received", "from child");
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/views/EventBus/eventBus.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="container">
+    <child></child>
+  </div>
+</template>
+
+<script>
+import Child from "./child";
+export default {
+  components: {
+    Child
+  },
+  created() {
+    // 变量保存
+    this.EventBus.message = "hello";
+    // 事件监听
+    this.EventBus.$on("received", function(val) {
+      console.log("received: " + val);
+      // -> 'received: from child'
+    });
+  }
+};
+</script>
